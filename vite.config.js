45fe24c7de892528309e8139630e6089f5fb0407
Abhishek Kumar <meabhsk@gmail.com>
2025-11-21 17:41:59 +0000

    fix vite base path for Render

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,11 @@ import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/floravision",
+  base: "./",       // <-- FIXED
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      /* eslint-disable-next-line no-undef */
       "@": path.resolve(__dirname, "./src"),
     },
   },
